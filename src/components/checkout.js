4e136469f0ccbea4e2f102ac28956d1fca470e7b
Kyle Gill <kylerobertgill@gmail.com>
2018-04-18 17:45:07 -0600

    aws lambda, styling

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -8,12 +8,12 @@ const amount = 2500
 const Checkout = class extends React.Component {
   state = {
     disabled: false,
-    buttonText: 'Buy Now',
+    buttonText: 'BUY NOW',
     paymentMessage: '',
   }
 
   resetButton() {
-    this.setState({ disabled: false, buttonText: 'Buy Now' })
+    this.setState({ disabled: false, buttonText: 'BUY NOW' })
   }
 
   componentDidMount() {
@@ -29,17 +29,25 @@ const Checkout = class extends React.Component {
 
   openStripeCheckout(event) {
     event.preventDefault()
-    this.setState({ disabled: true, buttonText: 'Waiting...' })
+    this.setState({ disabled: true, buttonText: 'WAITING...' })
     stripeHandler.open({
       name: 'Demo Product',
       amount: amount,
       description: 'A product well worth your time',
       token: token => {
         /* 
-        You can replace the url below with this one to run 
-        the netlify-lambda function in a dev environment:
-        fetch(`http://localhost:9000/purchase`, {
-        */
+        You can replace the url below to run the serverless function 
+        from a different environment.
+
+        run the netlify-lambda function in a dev environment:
+        `http://localhost:9000/purchase`
+        
+        run the netlify-lambda function from a prod environment:
+        `https://gatsby-stripe.netlify.com/.netlify/functions/purchase`
+        
+        run the aws-lambda function from a prod environment:
+        `https://4m5jfeec48.execute-api.us-east-1.amazonaws.com/dev/checkout`
+       */
         fetch(
           `https://4m5jfeec48.execute-api.us-east-1.amazonaws.com/dev/checkout`,
           {
@@ -75,25 +83,30 @@ const Checkout = class extends React.Component {
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'space-around',
-          textAlign: 'center',
+          alignItems: 'flex-start',
           padding: '3rem',
           boxShadow: '5px 5px 25px 0 rgba(46,61,73,.2)',
           backgroundColor: '#fff',
           borderRadius: '6px',
-          height: '250px',
+          maxWidth: '400px',
         }}
       >
-        <span>Spend your Money!</span>
+        <h4>Spend your Money!</h4>
+        <p>
+          Use any email, 4242 4242 4242 4242 as the credit card number, any 3
+          digit number, and any future date of expiration.
+        </p>
         <button
           style={{
-            margin: '0rem 1rem',
+            fontSize: '13px',
             boxShadow: '2px 5px 10px rgba(0,0,0,.1)',
             textAlign: 'center',
-            padding: '1rem 4rem',
+            padding: '12px 60px',
             backgroundColor: '#02b3e4',
             color: '#fff',
-            borderRadius: '3px',
+            borderRadius: '6px',
             outline: 'none',
+            letterSpacing: '1.5px',
           }}
           onClick={event => this.openStripeCheckout(event)}
           disabled={this.state.disabled}
